5e01afef79af02365b7e6f791f72e5f4009b2b03
Németh Tamás <tamas.nemeth2002@gmail.com>
2020-04-12 21:21:10 +0200

    security uppdate

---
+++ precache-manifest.5ac471ef694ca403eed74e8056b04002.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65de70868fdbb8f487a7e5a57ba32c23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23d8c361ecaf03078bdc",
+    "url": "/static/js/2.e05b7a72.chunk.js"
+  },
+  {
+    "revision": "14060d5403b5cb599a71704f7b391b06",
+    "url": "/static/js/2.e05b7a72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f720795c7d69e8b266fa",
+    "url": "/static/js/main.416a2894.chunk.js"
+  },
+  {
+    "revision": "b3846c4feb7507237cf1",
+    "url": "/static/js/runtime-main.7579e155.js"
+  },
+  {
+    "revision": "38ed3ef006e89f3ed20156f31bdb9e6d",
+    "url": "/static/media/me.38ed3ef0.jpg"
+  }
+]);
